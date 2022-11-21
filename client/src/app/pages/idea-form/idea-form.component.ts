6cd0a3dd2451d59dd34127d1211e01ee1fb56971
aliasad-it <103116702+aliasad-it@users.noreply.github.com>
2022-11-21 15:39:46 +0500

    Update idea-form.component.ts

--- client/src/app/pages/idea-form/idea-form.component.ts
+++ client/src/app/pages/idea-form/idea-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { FormBuilder, NgForm, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBuilder, NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Subscription, Observable } from 'rxjs';
 import { IdeaService } from 'src/app/services/idea.service';
 import { AdminService } from 'src/app/services/admin.service';
@@ -15,11 +15,11 @@ export class IdeaFormComponent implements OnInit {
   ideaForms: any;
   ideaForm: FormGroup;
   hasError: boolean;
-  ErrorMassage:string=' Kindly fill all the fields';
+  ErrorMassage: string = ' Kindly fill all the fields';
   returnUrl: string;
   isLoading$: Observable<boolean>;
-  department:any;
-  subject:String;
+  department: any;
+  subject: String;
   description: String;
   attachment: any;
   cat_id: String;
@@ -28,26 +28,27 @@ export class IdeaFormComponent implements OnInit {
   idea_status: any;
   updateby: any;
   userdata: any;
-  cate:any;
-  farea:any;
+  cate: any;
+  farea: any;
+  image: any;
 
   private unsubscribe: Subscription[] = [];
   constructor(
     public router: Router,
     private fb: FormBuilder,
     public ideasService: IdeaService,
-    public admin:AdminService
-    ) { }
+    public admin: AdminService
+  ) { }
 
   ngOnInit(): void {
-    
-    this.userdata=localStorage.getItem('user');
-    this.userdata=JSON.parse(this.userdata);        
+
+    this.userdata = localStorage.getItem('user');
+    this.userdata = JSON.parse(this.userdata);
     // this.router.getCurrentNavigation().extras.state
-    this.admin.getCategoryList(this.userdata).subscribe(data=>{
+    this.admin.getCategoryList(this.userdata).subscribe(data => {
       this.cate = data.data;
     })
-    this.admin.getFunctionList(this.userdata).subscribe(data=>{
+    this.admin.getFunctionList(this.userdata).subscribe(data => {
       this.farea = data.data;
     })
   }
@@ -55,25 +56,28 @@ export class IdeaFormComponent implements OnInit {
     return this.ideaForm.controls;
   }
 
+  selectImage(event:any) {
+    this.image = event.target.files[0];
+    console.log(this.image);
+    
+  }
+
   submit(form: NgForm) {
     form.value['updateby'] = Number(this.userdata.userid);
     this.hasError = false;
-   
-    this.ideasService.getSaveIdea(form.value).subscribe(data => {
-      if (data.status){
+    this.ideasService.getSaveIdea(form.value, this.image).subscribe(data => {
+      if (data.status) {
         this.router.navigateByUrl('/dashboard')
-       }else {
+      } else {
         this.hasError = true;
         this.ErrorMassage = 'Failed to submit.';
       }
+
       this.ideaForms = data.data;
-     
-    }); 
-      
-   
-    
-    
-    
+      console.log(this.ideaForms);
+
+    });
+
   }
 
   ngOnDestroy() {
